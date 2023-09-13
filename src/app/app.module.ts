6e937c7143b4d1464aeebfd0bdcf88626ba8d108
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-09-13 02:20:35 -0600

    components and services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TimesheetComponent,
+    DepartmentsComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
