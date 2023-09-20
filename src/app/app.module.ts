315d6ae666a7498bbd8904a9cee4bbf69f06174b
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-09-19 21:18:52 -0600

    unit 3 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './modules/app-routing.module';
-import { MaterialModule } from './modules/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DepartmentsComponent } from './components/departments/departments.component';
@@ -10,6 +8,7 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
