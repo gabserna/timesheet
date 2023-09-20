9762ddcb17ab8590845ab0631f00caa4e285301b
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-09-19 20:58:07 -0600

    unit 1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
+import { MaterialModule } from './modules/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { TimesheetComponent } from './components/timesheet/timesheet.component';
-import { DepartmentsComponent } from './components/departments/departments.component';
-import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    TimesheetComponent,
-    DepartmentsComponent,
-    ReportComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
