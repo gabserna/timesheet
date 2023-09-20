a43ced0617a1fc7486c3d3d208bec62aaad4396c
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-09-19 21:51:38 -0600

    unit 5 completed

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
 
 
 @NgModule({
@@ -12,11 +15,15 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatListModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
