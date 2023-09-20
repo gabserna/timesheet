8ba36561962d6fcdebc8bd02d6753852b5648465
gabserna <gabriel.sernapoot6740@stu.mtec.edu>
2023-09-19 21:05:58 -0600

    unit 2 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { MaterialModule } from './modules/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyDu2o_xS220g0lNqviIfi_QCucocBjlbnc',
+      authDomain: 'angularfire-462f8.firebaseapp.com',
+  
+      // databaseURL: '<your-database-URL>',
+  
+      projectId: 'angularfire-462f8',
+      storageBucket: 'angularfire-462f8.appspot.com',
+      messagingSenderId: '887283735920',
+    },
+  };
+
+//     appId: "",
+//     measurementId: ""
